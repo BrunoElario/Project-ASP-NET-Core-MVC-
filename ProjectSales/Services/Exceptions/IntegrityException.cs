9c22adebff8ffd65438d42212e3479c59b82d54f
BrunoElario <bruno.rheinheimer@esgnow.co>
2022-12-29 10:50:10 -0300

    exception-handling-for-delete

---
+++ ProjectSales/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectSales.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message) 
+        {
+        }
+
+    }
+}
